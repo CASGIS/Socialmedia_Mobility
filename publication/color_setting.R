7593c9d28fe46c16ef7ebb6bffbdf421026eb64f
Kaixin <kaixin_zhu@foxmail.com>
2023-06-25 13:57:13 +0800

    upload

---
+++ publication/color_setting.R
@@ -0,0 +1,8 @@
+
+# preset the color
+
+color_primary <- 
+  c("#f1c96c", "#ec8a5d", "#5ca076", "#5970bf", "#9163af", "#87bfdc")
+
+color_secondary <-
+  c("#f9e4b3", "#f5c3ac", "#a7ceb9", "#acb7df", "#c7b2d6", "#c1deed")
