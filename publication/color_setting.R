d963ea88d071b4e30d66ab63f7bc931789366403
Kaixin <kaixin_zhu@foxmail.com>
2023-06-25 16:40:43 +0800

    upload

--- publication/color_setting.R
+++ publication/color_setting.R
@@ -2,7 +2,7 @@
 # preset the color
 
 color_primary <- 
-  c("#f1c96c", "#ec8a5d", "#5ca076", "#5970bf", "#9163af", "#87bfdc")
+  c("#f1c96c", "#ec8a5d", "#5970bf", "#5ca076", "#9163af", "#87bfdc")
 
 color_secondary <-
-  c("#f9e4b3", "#f5c3ac", "#a7ceb9", "#acb7df", "#c7b2d6", "#c1deed")
+  c("#f9e4b3", "#f5c3ac", "#acb7df", "#a7ceb9", "#c7b2d6", "#c1deed")
